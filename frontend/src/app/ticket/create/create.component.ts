899b53d37f4e407247049287ca2bde10e3411393
Typlosion14 <pro-morganhue@outlook.fr>
2021-08-26 11:56:18 +0200

    merge

--- frontend/src/app/ticket/create/create.component.ts
+++ frontend/src/app/ticket/create/create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  emailExists=false;
+
+  TicketForm: FormGroup= new FormGroup({
+    description:new FormControl('',Validators.required)
+  })
+
+  saveUser(){
+    //Enregistrement de l'user
+    //environment.listUser.push(JSON.stringify(this.RegisterForm.value))
+    let user = <any>{
+
+    }
+    this.TicketService.create(user).subscribe((data)=>{
+      this.messageService.add({severity:'success', summary:'Message d\'inscription', detail:'Inscription réussie'});
+      this.router.navigate(['/login'])
+    },(error)=>{
+      if(error.status==400){
+        //Bad Request (Email déjà utilisé)
+        this.messageService.add({severity:'error', summary:'Message d\'inscription', detail:'Email déjà utilisé'});
+        this.emailExists=true;
+      }
+      console.log(error)
+    });
+    
+  }
+
+  get description() { return this.TicketForm.get('description'); }
+
+  constructor(private router: Router,private TicketService:TicketService,private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
