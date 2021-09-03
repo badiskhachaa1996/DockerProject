2b007ae154d05243d4b2a7c7ee8d7a651ed27d2f
Typlosion14 <pro-morganhue@outlook.fr>
2021-09-03 09:51:32 +0200

    Fix Merge

--- frontend/src/app/user1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { User1 } from './user1';
-
-describe('User1', () => {
-  it('should create an instance', () => {
-    expect(new User1()).toBeTruthy();
-  });
-});
--- frontend/src/app/user1.ts
+++
@@ -1,8 +0,0 @@
-
-export class User1 {
-    constructor(public idUser?:number,
-                public email?:String,
-                public nom?:String,
-                public prenom?:String,)
-                {}
-}
