1ca78b1d374680f3b8621fc23200a844189229a0
Typlosion14 <pro-morganhue@outlook.fr>
2021-09-08 10:25:16 +0200

    p-fileUpload fixed

--- frontend/src/app/ticket/list-ticket/list-ticket.component.ts
+++ frontend/src/app/ticket/list-ticket/list-ticket.component.ts
@@ -315,8 +315,8 @@ export class ListTicketComponent implements OnInit {
       ticket_id: this.selectedTicket._id,
       file: this.commentForm.value.file
     }
-
-    this.MsgServ.create(comment).subscribe((data) => {
+    console.log(comment)
+    /*this.MsgServ.create(comment).subscribe((data) => {
       this.messageService.add({ severity: 'success', summary: 'Gestion de message', detail: 'Creation de message réussie' });
       this.showFormAddComment = false;
       this.selectedTicket = null;
@@ -334,7 +334,7 @@ export class ListTicketComponent implements OnInit {
       console.log(data)
     }, (error) => {
       console.log(error)
-    })
+    })*/
   }
 
   downloadFile(message:Message) {
@@ -349,10 +349,11 @@ export class ListTicketComponent implements OnInit {
   }
 
   onFileChange(event) {
+
     let reader = new FileReader();
-    if (event.target.files && event.target.files.length > 0) {
+    if (event.files && event.files.length > 0) {
       this.loading = true
-      let file = event.target.files[0];
+      let file = event.files[0];
       reader.readAsDataURL(file);
       reader.onload = () => {
         this.commentForm.get('file').setValue({
