b1178dfc7fcc3209daec84a70bec9df75a420e88
TcHiZo <chadi.shwada@gmail.com>
2021-09-10 17:07:37 +0200

    Page notifs

--- frontend/src/app/notification/notification.component.ts
+++ frontend/src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NotificationService } from 'src/app/services/notification.service';
 import { TicketService } from '../services/ticket.service';
 import jwt_decode from "jwt-decode";
-import { AppTopBarComponent } from '../app.topbar.component';
+import { SujetService } from '../services/sujet.service';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
@@ -13,13 +13,14 @@ export class NotificationComponent implements OnInit {
 
   notifications: any = [];
   listTicket: any[] = [];
-  constructor(private NotificationService: NotificationService, private TicketService: TicketService) { }
+  sujetDic: any[] = [];
+  constructor(private NotificationService: NotificationService, private TicketService: TicketService,private SujetService:SujetService) { }
   // "Nouveau Ticket Affecté" "Modification d'un ticket" "Nouveau Message" "Traitement de votre ticket" "Revert d'un ticket"
 
   ngOnInit(): void {
     let token = localStorage.getItem("token")
     if (token) {
-      token=jwt_decode(token)
+      token = jwt_decode(token)
       this.NotificationService.get20ByUserID(token["id"])
         .subscribe(
           data => {
@@ -29,7 +30,7 @@ export class NotificationComponent implements OnInit {
           error => {
             console.log(error);
           });
-            
+
       this.TicketService.getAll()
         .subscribe(
           data => {
@@ -40,34 +41,35 @@ export class NotificationComponent implements OnInit {
           error => {
             console.log(error);
           });
+          this.SujetService.getAll().subscribe(data=>{
+            data.forEach(sujet => {
+              this.sujetDic[sujet._id]=sujet;
+            });
+          })
     }
-  
+
   }
-  retirer(notification): void{
-    
+  retirer(notification): void {
+
     this.NotificationService.viewNotifByID(notification._id)
-    .subscribe(
-      response => {
-        this.notifications.splice(this.notifications.indexOf(notification), 1);
-         if(this.notifications.length==0){
-           //this.topbar.notif=false
-        }
-      },
-      error => {
-        console.log(error);
-      });
-    }
-    retirertout(){
-      this.NotificationService.viewNotifs(this.notifications)
       .subscribe(
         response => {
-        
-    this.notifications=[];
-    //this.topbar.notif=false
+          this.notifications.splice(this.notifications.indexOf(notification), 1);
         },
         error => {
           console.log(error);
         });
-    }
+  }
+  retirertout() {
+    this.NotificationService.viewNotifs(this.notifications)
+      .subscribe(
+        response => {
+
+          this.notifications = [];
+        },
+        error => {
+          console.log(error);
+        });
+  }
 
 }
