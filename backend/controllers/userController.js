e4aa7d87bf350f6783e71dff46f02afee9a0f5d1
Typlosion14 <pro-morganhue@outlook.fr>
2021-09-16 11:30:42 +0200

    fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,8 +13,8 @@ let transporter = nodemailer.createTransport({
     secure: false, // true for 587, false for other ports
     requireTLS: true,
     auth: {
-        user: 'estya-ticketing@estya.com', 
-        pass: 'ESTYA@@2021', 
+        user: 'estya-ticketing@estya.com',
+        pass: 'ESTYA@@2021',
     },
 });
 
@@ -23,133 +23,123 @@ let transporter = nodemailer.createTransport({
 
 //service registre
 app.post("/registre", (req, res) => {
-   
-    let data = req.body;        
-   
-    console.log(data)
-        let user = new User({
+    let data = req.body;
+    let user = new User({
         civilite: data.civilite,
         firstname: data.firstname,
         lastname: data.lastname,
         phone: data.phone,
-        adresse:data.adresse,
+        adresse: data.adresse,
         email: data.email,
-        password: bcrypt.hashSync(data.password,8),
-      
-        role : data.role || "user",
-        service_id : data?.service_id || null
+        password: bcrypt.hashSync(data.password, 8),
+        role: data.role || "user",
+        service_id: data?.service_id || null
     })
-        
     user.save().then((userFromDb) => {
-        
         res.status(200).send(userFromDb);
-
-        let htmlmail='<p>Bonjour '+userFromDb.lastname+' '+userFromDb.firstname+', </p><p style="color:black"> <span style="color:orange">Felicitation ! </span> Votre compte E-Ticketing a été crée avec succés</p><p style="color:black">Cordialement,</p><footer> <img  src="red"/></footer>';
+        let htmlmail = '<p>Bonjour ' + userFromDb.lastname + ' ' + userFromDb.firstname + ', </p><p style="color:black"> <span style="color:orange">Felicitations ! </span> Votre compte E-Ticketing a été crée avec succés.</p><p style="color:black">Cordialement.</p><footer> <img  src="red"/></footer>';
         let mailOptions = {
             from: 'estya-ticketing@estya.com',
             to: data.email,
             subject: 'Estya-Ticketing',
-            html:htmlmail,
+            html: htmlmail,
             attachments: [{
                 filename: 'signature.png',
                 path: 'assets/signature.png',
                 cid: 'red' //same cid value as in the html img src
             }]
         };
-        
-        transporter.sendMail(mailOptions, function(error, info){
+        transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-               console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
+                console.log(error);
             }
         });
-        
     }).catch((error) => {
+        console.log(error)
         res.status(400).send(error);
     })
 });
 //service login
 app.post("/login", (req, res) => {
     let data = req.body;
-    User.findOne({ 
-        email: data.email, 
-        }).then((userFromDb) => {
-        comparer =  bcrypt.compareSync(data.password, userFromDb.password); 
-           console.log(userFromDb.password);
-            if (!userFromDb && !comparer) {
+    User.findOne({
+        email: data.email,
+    }).then((userFromDb) => {
+        comparer = bcrypt.compareSync(data.password, userFromDb.password);
+        if (!userFromDb && !comparer) {
             res.status(404).send({ message: data });
         }
         else {
-            let token = jwt.sign({ id: userFromDb._id, role: userFromDb.role ,service_id:userFromDb.service_id }, "mykey")
-            //on est entrain d'envoyer le token dans response donc dans le headers du coup dans le body on reçoit le msg logged
-            //res.set('token',token);
-            //res.status(200).send({message:"user logged"});
-            //on envoie dans le body le token
+            let token = jwt.sign({ id: userFromDb._id, role: userFromDb.role, service_id: userFromDb.service_id }, "mykey")
             res.status(200).send({ token });
         }
     }).catch((error) => {
-        res.status(404).send("erreur :" + error);
+        console.log(error)
+        res.status(404).send(error);
     })
 });
 app.get("/getById/:id", (req, res) => {
     let id = req.params.id;
     User.findOne({ _id: id }).then((userFromDb) => {
-        let userToken = jwt.sign({userFromDb},"userData")
+        let userToken = jwt.sign({ userFromDb }, "userData")
         res.status(200).send({ userToken });
     }).catch((error) => {
-        res.status(404).send("erreur :" + error);
+        console.log(error)
+        res.status(404).send(error);
     })
 });
-app.get("/getAll",(req,res)=>{
+app.get("/getAll", (req, res) => {
     User.find()
-    .then(result=>{
-        res.send(result.length>0?result:[]);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .then(result => {
+            res.send(result.length > 0 ? result : []);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404).send(error);
+        })
 });
 
 
 app.post("/updateById/:id", (req, res) => {
-    User.findOneAndUpdate({_id:req.params.id},
+    User.findOneAndUpdate({ _id: req.params.id },
         {
-            civilite:req.body.civilite,
-            firstname:req.body.firstname,
-            lastname:req.body.lastname,
-            phone:req.body.phone,
-            role:req.body.role,
-            adresse:req.body.adresse,
-            service_id:req.body.service_id
+            civilite: req.body.civilite,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            phone: req.body.phone,
+            role: req.body.role,
+            adresse: req.body.adresse,
+            service_id: req.body.service_id
 
         }, { new: true }, (err, user) => {
             if (err) {
+                console.log(err);
                 res.send(err)
+            }else{
+                res.send(user)
             }
-            res.send(user)
         })
 })
-app.get("/getAllbyService/:id",(req,res)=>{
-    User.find({service:req.params.id})
-    .then(result=>{
-        //console.log('result: ',result)
-        res.send(result.length>0?result:[]);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+app.get("/getAllbyService/:id", (req, res) => {
+    User.find({ service: req.params.id })
+        .then(result => {
+            res.send(result.length > 0 ? result : []);
+        })
+        .catch(err => {
+            res.status(404).send(error);
+            console.log(err);
+        })
 });
-app.get("/getAllAgent/",(req,res)=>{
-    User.find({role:["Responsable","Agent","Admin"] })
+app.get("/getAllAgent/", (req, res) => {
+    User.find({ role: ["Responsable", "Agent", "Admin"] })
 
-    .then(result=>{
-        //console.log('result: ',result)
-        res.send(result.length>0?result:[]);
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .then(result => {
+            res.send(result.length > 0 ? result : []);
+        })
+        .catch(err => {
+            res.status(404).send(error);
+            console.log(err);
+        })
 })
 
 
