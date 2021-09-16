ceb3167d3949278817424a0b1518bd31c47ecb12
malika199 <malika.derfoufii@gmail.com>
2021-09-16 10:57:02 +0200

    flech retour notif modifier profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,7 @@ app.post("/registre", (req, res) => {
         email: data.email,
         password: bcrypt.hashSync(data.password,8),
       
-        role : data.role || "user",
+        role : data.role || "User",
         service_id : data?.service_id || null
     })
         
