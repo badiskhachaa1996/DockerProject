d31259cddf8dfc6de8174452724f17180c84ce0c
Typlosion14 <pro-morganhue@outlook.fr>
2021-09-27 16:34:25 +0200

    Supprime ancienne photo de profil

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,8 +38,8 @@ app.post("/registre", (req, res) => {
     })
     user.save().then((userFromDb) => {
         res.status(200).send(userFromDb);
-        let gender = (userFromDb.civilite=='Monsieur')?'M. ':'Mme ';
-        let htmlmail = '<p>Bonjour '+ gender + userFromDb.lastname + ' ' + userFromDb.firstname + ', </p><p style="color:black"> <span style="color:orange">Felicitations ! </span> Votre compte E-Ticketing a été crée avec succés.</p><p style="color:black">Cordialement.</p><footer> <img  src="red"/></footer>';
+        let gender = (userFromDb.civilite == 'Monsieur') ? 'M. ' : 'Mme ';
+        let htmlmail = '<p>Bonjour ' + gender + userFromDb.lastname + ' ' + userFromDb.firstname + ', </p><p style="color:black"> <span style="color:orange">Felicitations ! </span> Votre compte E-Ticketing a été crée avec succés.</p><p style="color:black">Cordialement.</p><footer> <img  src="red"/></footer>';
         let mailOptions = {
             from: 'estya-ticketing@estya.com',
             to: data.email,
@@ -54,8 +54,8 @@ app.post("/registre", (req, res) => {
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
-            }else{
-                console.log("Email envoyé\nà "+userFromDb.email+"\nRaison:Création de compte")
+            } else {
+                console.log("Email envoyé\nà " + userFromDb.email + "\nRaison:Création de compte")
             }
         });
     }).catch((error) => {
@@ -124,7 +124,7 @@ app.post("/updateById/:id", (req, res) => {
             if (err) {
                 console.log(err);
                 res.send(err)
-            }else{
+            } else {
                 res.send(user)
             }
         })
@@ -156,22 +156,22 @@ app.get("/getAllAgent/", (req, res) => {
 })
 
 //Mise à jour du mot de passe
-app.post("/updatePassword/:id",(req,res)=>{
-    User.findById(req.params.id,(err,user)=>{
+app.post("/updatePassword/:id", (req, res) => {
+    User.findById(req.params.id, (err, user) => {
         comparer = bcrypt.compareSync(req.body.actualpassword, user.password)
-        if(comparer){
-            User.findOneAndUpdate({ _id: req.params.id },{
-                password:bcrypt.hashSync(req.body.password, 8)
+        if (comparer) {
+            User.findOneAndUpdate({ _id: req.params.id }, {
+                password: bcrypt.hashSync(req.body.password, 8)
             }, { new: true }, (err, user) => {
                 if (err) {
                     console.log(err);
                     res.send(err)
-                }else{
+                } else {
                     res.send(user)
                 }
             })
-        }else{
-            res.send({error:"Pas le bon mot de passe actuel"})
+        } else {
+            res.send({ error: "Pas le bon mot de passe actuel" })
         }
     })
 
@@ -185,54 +185,66 @@ const storage = multer.diskStorage({
     filename: (req, file, callBack) => {
         callBack(null, `${file.originalname}`)
     }
-  })
-  
+})
+
 const upload = multer({ storage: storage })
 
 //Sauvegarde de la photo de profile
 app.post('/file', upload.single('file'), (req, res, next) => {
     const file = req.file;
     if (!file) {
-      const error = new Error('No File')
-      error.httpStatusCode = 400
-      return next(error)
+        const error = new Error('No File')
+        error.httpStatusCode = 400
+        return next(error)
     }
-    User.findOneAndUpdate({ _id: req.body.id },{
-        pathImageProfil:file.filename,
-        typeImageProfil:file.mimetype
-    },(err,user)=>{
+    User.findById(req.body.id, (err, photo) => {
+        try {
+            fs.unlinkSync('storage/profile/' + photo.pathImageProfil)
+            //file removed
+        } catch (err) {
+            console.error(err)
+        }
+    })
+    User.findOneAndUpdate({ _id: req.body.id }, {
+        pathImageProfil: file.filename,
+        typeImageProfil: file.mimetype
+    }, (err, user) => {
         //Renvoie de la photo de profile au Front pour pouvoir l'afficher
-        res.send({message:"Photo mise à jour"});
-       
+        res.send({ message: "Photo mise à jour" });
+
     })
 
-  })
+})
 
-  //Envoie de la photo de profile
-app.get('/getProfilePicture/:id',(req,res)=>{
-    User.findById(req.params.id,(err,user)=>{
-        if(user && user.pathImageProfil){
-            let file = fs.readFileSync("storage/profile/" + user.pathImageProfil, { encoding: 'base64' }, (err) => {
-                if (err) {
-                    return console.error(err);
-                }
-            });
-            res.send({ file: file, documentType: user.typeImageProfil })
-        }else{
-            res.send({ error:"Image non défini" })
+//Envoie de la photo de profile
+app.get('/getProfilePicture/:id', (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+        if (user && user.pathImageProfil) {
+            try {
+                let file = fs.readFileSync("storage/profile/" + user.pathImageProfil, { encoding: 'base64' }, (err) => {
+                    if (err) {
+                        return console.error(err);
+                    }
+                });
+                res.send({ file: file, documentType: user.typeImageProfil })
+            }catch (e){
+                res.send({ error: "Image non trouvé" })
+            }
+        } else {
+            res.send({ error: "Image non défini" })
         }
 
     })
 })
 
-app.post('/AuthMicrosoft',(req,res)=>{
-    User.findOne({email:req.body.email},(err,user)=>{
-        if(user){
+app.post('/AuthMicrosoft', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (user) {
             let token = jwt.sign({ id: user._id, role: user.role, service_id: user.service_id }, "mykey")
             res.status(200).send({ token });
-        }else{
-            let lastname = req.body.name.substring(req.body.name.indexOf(" ")+1); //Morgan HUE
-            let firstname = req.body.name.replace(" "+lastname,'')
+        } else {
+            let lastname = req.body.name.substring(req.body.name.indexOf(" ") + 1); //Morgan HUE
+            let firstname = req.body.name.replace(" " + lastname, '')
             let user = new User({
                 firstname: firstname,
                 lastname: lastname,
@@ -242,7 +254,7 @@ app.post('/AuthMicrosoft',(req,res)=>{
             })
             user.save().then((userFromDb) => {
                 let token = jwt.sign({ id: userFromDb._id, role: userFromDb.role, service_id: userFromDb.service_id }, "mykey")
-                res.status(200).send({ token ,message:"Nouveau compte crée"});
+                res.status(200).send({ token, message: "Nouveau compte crée" });
             })
         }
     })
