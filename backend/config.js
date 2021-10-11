651386537642895ea618df944f414efb1023dd2f
Typlosion14 <pro-morganhue@outlook.fr>
2021-10-11 11:03:26 +0200

    Cors fix

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-const origin = "https://ticket.estya.com/" //"http://localhost:4200"
+const origin = "https://ticket.estya.com" //"http://localhost:4200" https://ticket.estya.com
 
 module.exports=origin;
