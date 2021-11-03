7379d5841d7269450c5b949b9855b1c4b70f0943
Soualmia Hachem <soualmia.hachem@gmail.com>
2021-11-03 16:25:11 +0000

    Update backend/config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
-const origin = "http://localhost:4200" //"http://localhost:4200" https://ticket.estya.com
-
+const origin = "http://localhost:4200" //"https://ticket.estya.com/"
 module.exports=origin;
