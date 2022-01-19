567712fbf42bd19ff4998713ef9b109ad48fb787
BABACI Slimane <babacislimane@gmail.com>
2022-01-19 10:34:12 +0100

    preinscription tableau

--- backend/controllers/inscriptionController.js
+++ backend/controllers/inscriptionController.js
@@ -2,11 +2,26 @@ const express = require("express");
 const app = express();
 const { Inscription } = require("./../models/inscription");
 const { User } = require("./../models/user");
+const nodemailer = require('nodemailer');
+
+
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false, // true for 587, false for other ports
+    requireTLS: true,
+    auth: {
+        user: 'estya-ticketing@estya.com',
+        pass: 'ESTYA@@2021',
+    },
+});
+
 
 app.post("/create", (req, res) => {
     //Ajouter une inscription pour un user existant ( réinscription)
     let data = req.body;
-  
+
     let inscription = new Inscription({
         user_id: null,
         statut: data.statut,
@@ -24,60 +39,84 @@ app.post("/create", (req, res) => {
 app.post("/firstInscription", (req, res) => {
 
     let data = req.body.fInscription;
-    console.log("ici"),
-    console.log(data.diplome)
+
 
     let inscription = new Inscription({
-       
+
         user_id: null,
-        statut:  "data.statut1",
+        statut: data.statut,
         diplome: data.diplome
     });
 
     //Creation du nouveau user
     let userData = req.body.newUser;
-  
+
     let user = new User({
-        
-            civilite:null,
-            firstname: userData.firstname,
-            lastname: userData.lastname,
-            phone: null,
-            email: userData.email_perso,
-            /*password: bcrypt.hashSync(data.password, 8),*/
-            role:"user",
-            service_id: null,
-            type: null,
-            entreprise: null,
-            pays_adresse: null,
-            ville_adresse: null,
-            rue_adresse: null,
-            numero_adresse: null,
-            postal_adresse: null
-        });
+
+        civilite: null,
+        firstname: userData.firstname,
+        lastname: userData.lastname,
+        phone: null,
+        email: userData.email_perso,
+        email_perso: userData.email_perso,
+        /*password: bcrypt.hashSync(data.password, 8),*/
+        role: "user",
+        service_id: null,
+        type: null,
+        entreprise: null,
+        pays_adresse: null,
+        ville_adresse: null,
+        rue_adresse: null,
+        numero_adresse: null,
+        postal_adresse: null
+    });
     //vérification de l'existence du user
     User.findOne({ email_perso: userData.email_perso })
         .then((userFromDb) => {
             if (userFromDb) {
-    console.log(userFromDb)
+                console.log(userFromDb)
 
                 inscription.user_id = userFromDb.user_id;
-              //  console.log("ici user_id =" ,inscription.user_id)
+                //  console.log("ici user_id =" ,inscription.user_id)
                 inscription.save().then((InscriptionFromDb) => { res.status(201).json({ success: "Inscription ajouté !" }) })
                     .catch((error) => { res.status(400).json({ error: "Impossible d'ajouter l'inscription " + error }) });
             }
-        
-    else {
-        user.save().then((userCreated) => {
-            inscription.user_id = userCreated._id;
-            inscription.save()
-            .then((inscriptionCreated) => { res.status(201).json({ success: 'Inscription crée' }) })
-            .catch((error) => { res.status(400).json({ error: 'Impossible de crée cette inscription' }) });
-    })
-    .catch((error) => { res.status(400).json({ error: 'Impossible de créer un nouvel utilisateur ' + error.message }) });
+
+            else {
+                user.save().then((userCreated) => {
+                    inscription.user_id = userCreated._id;
+                    inscription.save()
+                        .then((inscriptionCreated) => {
+                            res.status(201).json({ success: 'Inscription crée' })
+
+
+
+
+                            let htmlmail = '<p>Bonjour, </p><p style="color:black"> <span style="color:orange">Felicitations ! </span> Votre Préinscription ESTYA Unibersity a été enregistré avec succés.</p><p style="color:black">Connectez vous avec votre email perso pour suivre les étapes d\'inscription .</p><p style="color:black">Cordialement.</p><footer> <img  src="red"/></footer>';
+                            let mailOptions = {
+                                from: 'estya-ticketing@estya.com',
+                                to: userCreated.email_perso,
+                                subject: 'Estya-EMS',
+                                html: htmlmail,
+                                attachments: [{
+                                    filename: 'signature.png',
+                                    path: 'assets/signature.png',
+                                    cid: 'red' //same cid value as in the html img src
+                                }]
+                            };
+                            transporter.sendMail(mailOptions, function (error, info) {
+                                if (error) {
+                                    console.error(error);
+                                }
+                            });
+                        }
+                        )
+                        .catch((error) => { res.status(400).json({ error: 'Impossible de crée cette inscription' }) });
+                })
+                    .catch((error) => { res.status(400).json({ error: 'Impossible de créer un nouvel utilisateur ' + error.message }) });
             }
         })
-        .catch ((error) => { res.status(500).json({ error: 'Impossible de verifier l\'existence de l\'utilisateur ' }) });
+        .catch((error) => { res.status(500).json({ error: 'Impossible de verifier l\'existence de l\'utilisateur ' }) });
 
 });
 //Modifier une inscription par ID
@@ -96,25 +135,56 @@ app.post("/editById/:id", (req, res) => {
         }).catch((error) => {
             res.status(400).send(error);
         });
-    })
-        
-    app.get("/getAll", (req, res) => {
-        //Récupérer toutes les inscription
-        Inscription.find().then(result => {
-            res.send(result.length > 0 ? result : [])
-        })
-            .catch((error) => {
-              //  console.error(error);
-            })
-    });
+})
 
-    app.get("/getById/:id", (req, res) => {
-        //Récupérer une école via un ID
-        Inscription.findOne({ _id: req.params.id }).then((dataInscription) => {
-            res.status(200).send({ dataInscription });
-        }).catch((error) => {
-            res.status(404).send(error);
-        })
+app.get("/getAll", (req, res) => {
+    //Récupérer toutes les inscription
+    Inscription.find().then(result => {
+        res.send(result.length > 0 ? result : [])
     })
+        .catch((error) => {
+            //  console.error(error);
+        })
+});
+
+app.get("/getById/:id", (req, res) => {
+    //Récupérer une école via un ID
+    Inscription.findOne({ _id: req.params.id }).then((dataInscription) => {
+        res.status(200).send({ dataInscription });
+    }).catch((error) => {
+        res.status(404).send(error);
+    })
+})
+
+app.get("/uploadFile/:id", (req, res) => {
+fs.mkdir("./storage/" +"Préinscription" + "/",
+{ recursive: true }, (err) => {
+    if (err) {
+        console.error(err);
+    }
+});
+fs.writeFile("storage/" +"Préinscription" + "/" + req.body.file.filename, req.body.file.value, 'base64', function (err) {
+if (err) {
+    console.error(err);
+}})
+});
+
+
+
+app.get("/downloadFile/:id", (req, res) => {
+    Inscription.findOne({ _id: req.params.id }).then((data) => {
+        let filename = data.document
+        let file = fs.readFileSync("storage/"+"Préinscription" + "/" + data._id + "/" + filename, { encoding: 'base64' }, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+        });
+        res.status(200).send({ file: file, documentType: data.documentType })
+    }).catch((error) => {
+        res.status(404).send("erreur :" + error);
+    })
+});
+
+
 
 module.exports = app;
--- frontend/src/app/first-inscription/first-inscription.component.ts
+++ frontend/src/app/first-inscription/first-inscription.component.ts
@@ -7,6 +7,12 @@ import { Inscription } from '../models/Inscription';
 import { User } from '../models/User';
 import { DiplomeService } from '../services/diplome.service';
 import { FirstInscriptionService } from '../services/first-inscription.service';
+import { AuthService } from '../services/auth.service';
+import  jwt_decode from 'jwt-decode';
+import {DialogModule} from 'primeng/dialog';
+import {FileUploadModule} from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-first-inscription',
@@ -15,9 +21,12 @@ import { FirstInscriptionService } from '../services/first-inscription.service';
 })
 export class FirstInscriptionComponent implements OnInit {
 
-  
+  display: boolean = false;
+
+ 
+
   inscriptions: Inscription[] = [];
-  diplomes : Diplome[]=[];
+  diplomes: Diplome[] = [];
   formAddNewInscription: FormGroup = new FormGroup({
     firstname: new FormControl('', [Validators.required]),
     lastname: new FormControl('', [Validators.required]),
@@ -30,45 +39,90 @@ export class FirstInscriptionComponent implements OnInit {
 
   idInscriptionToUpdate: string;
   users: User[] = [];
+  Selectedinscription: Inscription;
+  userSelected: User;
+  userDic:any= {};
+  diplomeDic:any={};
 
-  constructor(private diplomeService : DiplomeService,private fInscriptionService: FirstInscriptionService, private formBuilder: FormBuilder, private messageService: MessageService, private router: Router) { }
+  constructor(private diplomeService: DiplomeService, private fInscriptionService: FirstInscriptionService, private formBuilder: FormBuilder, private messageService: MessageService, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
 
     this.fInscriptionService.getAll().subscribe(
-      (data) => { this.inscriptions = data;
-      console.log(this.inscriptions) },
+      (data) => {
+        this.inscriptions = data;
+        console.log(this.inscriptions)
+
+      },
+      (error) => { console.log(error) }
+    );
+    this.authService.getAll().subscribe((data) => {
+
+      if (!data.message) {
+
+        data.forEach(user => {
+
+          this.userDic[user._id] = user;
+        });
+      }
+    })
+    this.diplomeService.getAll().subscribe(
+      (diplomeData) => {
+        this.diplomes = diplomeData
+        console.log(this.diplomes)
+        diplomeData.forEach(diplome =>{
+          this.diplomeDic[diplome._id] = diplome;
+        })
+      },
       (error) => { console.log(error) }
     );
 
-    this.diplomeService.getAll().subscribe(
-      (diplomeData)=>{this.diplomes=diplomeData
-      console.log(this.diplomes)},
-      (error) => { console.log(error)}
-    );
+  }
+  toggleFormfInscriptionAdd() {
+
+    this.showFormAddnewInscription = !this.showFormAddnewInscription;
+  }
+
+  createfInscription() {
+    console.log(this.formAddNewInscription.value.diplomeID._id)
+    let fInscription = new Inscription(null, null, this.formAddNewInscription.value.diplomeID._id, "préinscription")
+    let newUser = new User(null, this.formAddNewInscription.value.firstname, this.formAddNewInscription.value.lastname, null, null, this.formAddNewInscription.value.emailperso, "user", null, null, null, null, null, null, null, null, null, null, null, null)
   
-      }
-      toggleFormfInscriptionAdd(){
-
-        this.showFormAddnewInscription=!this.showFormAddnewInscription;
-      }
-
-      createfInscription(){
-        console.log(this.formAddNewInscription.value.diplomeID._id)
-        let fInscription= new Inscription(null,null,this.formAddNewInscription.value.diplomeID._id,"statut_test")
-        let newUser = new User(null,this.formAddNewInscription.value.firstname,this.formAddNewInscription.value.lastname,null,null,this.formAddNewInscription.value.emailperso,"user",null,null,null,null,null,null,null,null,null,null,null,null)
-        console.log(this.formAddNewInscription.value.firstname)
-        this.fInscriptionService.create({ 'newUser': newUser, 'fInscription': fInscription }).subscribe((data) => {
-          this.messageService.add({ severity: 'success', summary: 'Gestion des Inscriptions', detail: 'Une nouvelle inscription a été ajouté avec Un nouvel utilisateur' });
-          this.inscriptions.push(data)
-          this.showFormAddnewInscription=false;
-          this.formAddNewInscription.reset();
-        }, (error) => {
-          console.error(error)
-        });
+    this.fInscriptionService.create({ 'newUser': newUser, 'fInscription': fInscription }).subscribe((data) => {
       
-  
-        
-      }
+ 
+      this.inscriptions.push(data)
+      this.messageService.add({ severity: 'success', summary: 'Gestion des Inscriptions', detail: 'Une nouvelle inscription a été ajouté avec Un nouvel utilisateur' });
+      this.showFormAddnewInscription = false;
+      this.formAddNewInscription.reset();
+    }, (error) => {
+      console.error(error)
+    });
 
+  }
+
+  onRowSelect() {
+  
+    console.log(this.Selectedinscription.user_id)
+this.authService.getById(this.Selectedinscription.user_id).subscribe(
+  (userdata) => {
+
+    
+    this.userSelected = jwt_decode(userdata.userToken)["userFromDb"];
+    console.log(this.userSelected)
+    
+  }, (error) => { console.log(error) }
+  );
+    
+  }
+ showDialog(user) {
+      this.authService.getById(user.user_id).subscribe(
+        (userdata) => {
+          this.userSelected = jwt_decode(userdata.userToken)["userFromDb"];
+          console.log(this.userSelected)
+          
+        }, (error) => { console.log(error) }
+        );
+        this.display = true;
+  }
 }
