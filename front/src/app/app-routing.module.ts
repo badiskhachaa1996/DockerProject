3c15d595a0c377ca4ed7111bdeda1d6b0e01108a
BABACI Slimane <babacislimane@gmail.com>
2022-06-30 16:53:19 +0200

    guard dashboard

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -84,7 +84,7 @@ import { ProspectsComponent } from './pedagogie/prospects/prospects.component';
             {
                 path: '', component: AppMainComponent,
                 children: [
-                    { path: '', component: DashboardComponent },
+                    { path: '', component: DashboardComponent ,canActivate: [AuthGuardService]},
                     { path: 'uikit/formlayout', component: FormLayoutComponent },
                     { path: 'uikit/input', component: InputComponent },
                     { path: 'uikit/floatlabel', component: FloatLabelComponent },
