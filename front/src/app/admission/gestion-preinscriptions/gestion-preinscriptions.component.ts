c4484dad22a9c32d3cf7731b52b34265f5073cd1
Typlosion Flamme <pro-morganhue@outlook.fr>
2022-07-26 17:47:14 +0200

    Remarque Haythem

--- front/src/app/admission/gestion-preinscriptions/gestion-preinscriptions.component.ts
+++ front/src/app/admission/gestion-preinscriptions/gestion-preinscriptions.component.ts
@@ -192,11 +192,8 @@ export class GestionPreinscriptionsComponent implements OnInit {
   ngOnInit(): void {
     this.token = jwt_decode(localStorage.getItem("token"))
     this.userService.WhatTheRole(this.token.id).subscribe(data => {
-      if (data.type == 'Commercial' && data.data.statut == 'Admin') {
-        this.dataCommercial = data.data
-        console.log(data)
-      }
       if (data.type == "Commercial" && data.data.code_commercial_partenaire) {
+        this.dataCommercial = data.data
         localStorage.setItem("CommercialCode", data.data.code_commercial_partenaire)
       }
       this.refreshProspect()
