e1c30c129022f697abaef33a4560479dd949a071
unknown <chadidu93@gmail.com>
2022-07-28 14:38:07 +0200

    update form complétion profil

--- front/src/app/pedagogie/etudiants/add-etudiant/add-etudiant.component.ts
+++ front/src/app/pedagogie/etudiants/add-etudiant/add-etudiant.component.ts
@@ -175,7 +175,7 @@ export class AddEtudiantComponent implements OnInit {
   //Methode d'initialisation du formulaire d'ajout d'un étudiant
   onInitFormAddEtudiant() {
     this.formAddEtudiant = this.formBuilder.group({
-      civilite: [this.civiliteList[0]],
+      civilite: [this.civiliteList[0], Validators.required],
       firstname: ['', [Validators.required, Validators.pattern('[^0-9]+')]],
       lastname: ['', [Validators.required, Validators.pattern('[^0-9]+')]],
       indicatif: ['', [Validators.required]],
