3057b9d63161c0a04e1a1406c5f850b84c8e9337
Typlosion Flamme <pro-morganhue@outlook.fr>
2022-08-02 17:11:29 +0200

    Contrat Alternance

---
+++ back/models/contrat_alternance.js
@@ -0,0 +1,20 @@
+//importation de la bibliothèque mongoose
+const mongoose = require('mongoose');
+//creation du schema de la table diplome
+const contrat_alternanceSch = mongoose.Schema({
+    debut_contrat: { type: Date, required: true },
+    fin_contrat: { type: Date, required: true },
+    horaire: { type: String },
+    alternant_id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    intitule: { type: String, required: true },
+    classification: { type: String, required: true },
+    niveau_formation: { type: String, required: true },
+    coeff_hierachique: { type: String, required: true },
+    formation: { type: mongoose.Schema.Types.ObjectId, ref: "diplome" },
+    tuteur: { type: [mongoose.Schema.Types.ObjectId], ref: "user" }
+});
+
+//creation de la table avec le nom Diplome ( model/classe) à l'aide de la biblio mongoose et son schema
+const CAlternance = mongoose.model("contrat_alternance", contrat_alternanceSch);
+//on prépare ce model pour l'importer dans d'autres composants en l'exportant comme ça
+module.exports = { CAlternance };
