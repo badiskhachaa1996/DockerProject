b31fa77858fdbf0669793757c83577b0139521cc
Typlosion Flamme <pro-morganhue@outlook.fr>
2022-08-12 16:18:22 +0200

    Microsoft files 2

--- back/scriptImport/microsoftAPI.js
+++ back/scriptImport/microsoftAPI.js
@@ -14,54 +14,68 @@ var fileOptions = {
     fileName: "test.txt",
     fileContent: "JE SUIS UN TEST",
 }*/
-let fileList = fs.readdirSync('../storage')
-// storage/prospect/id/filetype/image.png
-fileList.forEach(file => {
-    if (fs.lstatSync('../storage/' + file).isDirectory()) {
-        let fileList2 = fs.readdirSync('../storage/' + file)
-        fileList2.forEach(file2 => {
-            if (fs.lstatSync('../storage/' + file + "/" + file2).isDirectory()) {
-                let fileList3 = fs.readdirSync('../storage/' + file + "/" + file2)
-                fileList3.forEach(file3 => {
-                    if (fs.lstatSync('../storage/' + file + "/" + file2 + "/" + file3).isDirectory()) {
-                        let fileList4 = fs.readdirSync('../storage/' + file + "/" + file2 + "/" + file3)
-                        fileList4.forEach(file4 => {
-                            if (fs.lstatSync('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4).isDirectory()) {
-                                console.log('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4 + "/")
-                            } else {
-                                sendFile({
-                                    folder: "Shared Documents/" + file + "/" + file2 + "/" + file3,
-                                    fileName: file4,
-                                    fileContent: fs.readFileSync('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4)
-                                }, [file, file2, file3])
-                            }
-                        });
-                    } else {
-                        sendFile({
-                            folder: "Shared Documents/" + file + "/" + file2,
-                            fileName: file3,
-                            fileContent: fs.readFileSync('../storage/' + file + "/" + file2 + "/" + file3)
-                        }, [file, file2])
-                    }
-                });
-            } else {
-                sendFile({
-                    folder: "Shared Documents/" + file,
-                    fileName: file2,
-                    fileContent: fs.readFileSync('../storage/' + file + "/" + file2)
-                }, [file])
-            }
-        });
-    } else {
-        console.log('../storage/' + file)
-    }
-});
-exit
+User.find().then(data => {
+    let userDic = {}
+    data.forEach(u => {
+        userDic[u._id] = u.lastname + " " + u.firstname
+    })
+    let fileList = fs.readdirSync('../storage')
+    // storage/prospect/id/filetype/image.png
+    fileList.forEach(file => {
+        if (fs.lstatSync('../storage/' + file).isDirectory()) {
+            let fileList2 = fs.readdirSync('../storage/' + file)
+            fileList2.forEach(file2 => {
+                if (fs.lstatSync('../storage/' + file + "/" + file2).isDirectory()) {
+                    let fileList3 = fs.readdirSync('../storage/' + file + "/" + file2)
+                    fileList3.forEach(file3 => {
+                        if (fs.lstatSync('../storage/' + file + "/" + file2 + "/" + file3).isDirectory()) {
+                            let fileList4 = fs.readdirSync('../storage/' + file + "/" + file2 + "/" + file3)
+                            fileList4.forEach(file4 => {
+                                if (fs.lstatSync('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4).isDirectory()) {
+                                    console.log('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4 + "/")
+                                } else {
+                                    let fileContent = fs.readFileSync('../storage/' + file + "/" + file2 + "/" + file3 + "/" + file4)
+                                    sendFile({
+                                        folder: "Shared Documents/" + file + "/" + file2 + "/" + file3,
+                                        fileName: file4,
+                                        fileContent
+                                    }, [file, file2, file3])
+                                }
+                            });
+                        } else {
+                            let fileContent = fs.readFileSync('../storage/' + file + "/" + file2 + "/" + file3)
+                            sendFile({
+                                folder: "Shared Documents/" + file + "/" + file2,
+                                fileName: file3,
+                                fileContent
+                            }, [file, file2])
+                        }
+                    });
+                } else {
+                    let fileContent = fs.readFileSync('../storage/' + file + "/" + file2)
+                    sendFile({
+                        folder: "Shared Documents/" + file,
+                        fileName: file2,
+                        fileContent
+                    }, [file])
+                }
+            });
+        } else {
+            console.log('../storage/' + file)
+        }
+    });
+})
+
 function sendFile(fileOptions, filePath) {
     var credentialOptions = {
         username: "test.admin@estya.com",
         password: "EstyaFR2022"
     }
+    filePath.forEach(file => {
+        if (userDic[file]) {
+            fileOptions.folder = fileOptions.folder.replace(file, userDic[file])
+        }
+    })
     spsave({
         siteUrl: "https://elitechgroupe.sharepoint.com/sites/Ims_storage"
     },
