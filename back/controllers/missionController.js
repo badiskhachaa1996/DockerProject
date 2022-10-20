10dba492a0d9ce46e68b7c11d51f6ecea081a927
Typlosion Flamme <pro-morganhue@outlook.fr>
2022-10-20 14:06:03 +0200

    Meilleur choix d'icones

--- back/controllers/missionController.js
+++ back/controllers/missionController.js
@@ -2,46 +2,61 @@ const express = require("express");
 const app = express();
 app.disable("x-powered-by");
 const { Mission } = require('./../models/Mission');
-
+const { CV } = require('./../models/cv')
 //Création d'une mission
 app.post("/post-mission", (req, res) => {
     const mission = new Mission({ ...req.body });
 
     mission.save()
-           .then((missionSaved) => { res.status(201).send(missionSaved); })
-           .catch((error) => { res.status(400).send(error.message); });
+        .then((missionSaved) => { res.status(201).send(missionSaved); })
+        .catch((error) => { res.status(400).send(error.message); });
 });
 
 
 //Recuperation de la liste des missions
 app.get("/get-missions", (_, res) => {
     Mission.find()
-           .then((missions) => { res.status(200).send(missions); })
-           .catch((error) => { res.status(500).send(error.message); });
+        .then((missions) => { res.status(200).send(missions); })
+        .catch((error) => { res.status(500).send(error.message); });
 });
 
 
 //Récuperation d'une mission via on identifiant
 app.get("/get-mission", (req, res) => {
     Mission.findById(req.params.missionId)
-           .then((mission) => { res.status(200).send(mission); })
-           .catch((error) => { res.status(500).send(error.message); });
+        .then((mission) => { res.status(200).send(mission); })
+        .catch((error) => { res.status(500).send(error.message); });
 });
 
 
 //Recuperation de la liste des mission d'un utilisateur
 app.get("/get-missions-by-user-id/:user_id", (req, res) => {
     Mission.find({ user_id: req.params.user_id })
-           .then((missions) => { res.status(200).send(missions); })
-           .catch((error) => { res.status(500).send(error.message); });
+        .then((missions) => { res.status(200).send(missions); })
+        .catch((error) => { res.status(500).send(error.message); });
 });
 
 
 //Recuperation de la liste des mission d'une entreprise
 app.get("/get-missions-by-entreprise-id/:entreprise_id", (req, res) => {
     Mission.find({ entreprise_id: req.params.entreprise_id })
-           .then((missions) => { res.status(200).send(missions); })
-           .catch((error) => { res.status(500).send(error.message); });
+        .then((missions) => { res.status(200).send(missions); })
+        .catch((error) => { res.status(500).send(error.message); });
 });
+
+//Recupération d'une liste de mission qui corresponds à un CV
+app.get('/getMissionFromCV/:cv_id', (req, res) => {
+    CV.findById(req.params.cv_id).then(cv => {
+        let langues = cv.langues
+        let skills = []
+        cv.connaissances.forEach(element => {
+            skills.push(element.skill)
+        });
+        cv.experiences.forEach(element => {
+            skills.push(element.skill)
+        });
+        
+    })
+})
 
 module.exports = app;
