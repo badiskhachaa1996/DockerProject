a55bd996013e90f17bde2fd1b4b92f8bb1115c04
idrissa-sall <sl.idriss10@gmail.com>
2022-11-18 16:27:11 +0100

    Ajout de la possibilité pour les collaborateurs d'ajouter le compte rendu d'activité

--- back/models/InTime.js
+++ back/models/InTime.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const inTimeSchema = mongoose.Schema({
-    user_id:            { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: false },
-    in_ip_adress:       { type: String, required: false }, //Adresse ip de l'utilisateur
-    out_ip_adress:      { type: String, required: false }, //Adresse ip de l'utilisateur
-    date_of_the_day:    { type: String, required: false }, //Stoque la date du jour sans l'heure
-    in_date:            { type: Date, required: false },
-    out_date:           { type: Date, required: false },
-    statut:             { type: String, required: false },
-    isCheckable:        { type: Boolean, required: false },
+    user_id:                        { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: false },
+    in_ip_adress:                   { type: String, required: false }, //Adresse ip de l'utilisateur
+    out_ip_adress:                  { type: String, required: false }, //Adresse ip de l'utilisateur
+    date_of_the_day:                { type: String, required: false }, //Stoque la date du jour sans l'heure
+    in_date:                        { type: Date, required: false },
+    out_date:                       { type: Date, required: false },
+    statut:                         { type: String, required: false },
+    isCheckable:                    { type: Boolean, required: false },
+    principale_activity_details:    { type: mongoose.Schema.Types.Mixed, required: false },
+    activity_details:               { type: [String], required: false },
 });
 
 const InTime = mongoose.model('intime', inTimeSchema);
