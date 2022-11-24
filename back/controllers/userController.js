d423a8d3c1d6d2222bfa1bce330852886f849044
Typlosion Flamme <pro-morganhue@outlook.fr>
2022-11-24 18:01:52 +0100

    Fix bouton en plus

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1006,19 +1006,18 @@ app.get('/cleanModel', (req, res) => {
 app.get('/deleteDuplicateProspect', async (req, res) => {
     //Supprimer les doublons de prospects
     let agg = await User.aggregate([
-        { "$group": { "_id": "$email_perso", "type": "$type", "count": { "$sum": 1 } } },
-        { "$match": { "_id": { "$ne": null }, $or: [{ type: 'Prospect' }, { type: null }], "count": { "$gt": 1 } } },
+        { "$group": { "_id": "$email_perso", "count": { "$sum": 1 } } },
+        { "$match": { "_id": { "$ne": null }, "count": { "$gt": 1 } } },
         { "$project": { "email_perso": "$_id", "_id": 0 } }
     ])
     let r = []
     for (const doc of agg) {
         if (doc.email_perso != '' && doc.email_perso != ' ' && doc.email_perso != null)
-            await User.findOneAndRemove({ email_perso: doc.email_perso }, { new: true }, (err, doc) => {
+            await User.findOneAndRemove({ email_perso: doc.email_perso, $or: [{ 'type': 'Prospect' }, { 'type': null }] }, { new: true }, (err, doc) => {
                 r.push(doc)
             })
     }
     res.status(201).send(r)
-
 })
 
 module.exports = app;
