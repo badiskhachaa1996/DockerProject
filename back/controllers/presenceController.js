7bd4703bfb4123441ff2397b466d9ea06accfebc
Typlosion Flamme <pro-morganhue@outlook.fr>
2023-02-07 15:54:16 +0100

    Fix erreur présence assiduité

--- back/controllers/presenceController.js
+++ back/controllers/presenceController.js
@@ -33,7 +33,7 @@ app.get("/getAll", (req, res) => {
 //Récupérer tous les presence d'un user
 app.get("/getAllByUser/:id", (req, res) => {
     let date = new Date(2023, 0, 1)
-    Presence.find({ user_id: req.params.id }, { date_signature: { $gt: date  } }).populate("seance_id").then((data) => {
+    Presence.find({ user_id: req.params.id, date_signature: { $gt: date } }).populate("seance_id").then((data) => {
         res.status(200).send(data);
     }).catch((error) => {
         console.error(error)
