61555604c993744bcdbd56af33035544fd82a3ba
Typlosion Flamme <pro-morganhue@outlook.fr>
2023-03-10 10:32:43 +0100

    Export Resulat QS

--- front/src/app/pedagogie/questionnaire-satisfaction/resultat/resultat.component.ts
+++ front/src/app/pedagogie/questionnaire-satisfaction/resultat/resultat.component.ts
@@ -3,7 +3,7 @@ import { QSService } from 'src/app/services/qs.service';
 import { MessageService } from 'primeng/api';
 import { DiplomeService } from 'src/app/services/diplome.service';
 import { ClasseService } from 'src/app/services/classe.service';
-
+import * as html2pdf from 'html2pdf.js';
 @Component({
   selector: 'app-resultat',
   templateUrl: './resultat.component.html',
@@ -99,21 +99,21 @@ export class ResultatComponent implements OnInit {
 
   updateMoyenne(data) {
     this.moyenne = {
-      horaire: Math.trunc(((data.reduce((total, next) => total + next.horaire, 0) / data.length)*100)/5),
-      charge: Math.trunc(((data.reduce((total, next) => total + next.charge, 0) / data.length)*100)/5),
-      satisfait_nb_matiere: Math.trunc(((data.reduce((total, next) => total + next.satisfait_nb_matiere, 0) / data.length)*100)/5),
-      satisfait_programme: Math.trunc(((data.reduce((total, next) => total + next.satisfait_programme, 0) / data.length)*100)/5),
-      satisfait_pedagogie_enseignant: Math.trunc(((data.reduce((total, next) => total + next.satisfait_pedagogie_enseignant, 0) / data.length)*100)/5),
-      satisfait_support: Math.trunc(((data.reduce((total, next) => total + next.satisfait_support, 0) / data.length)*100)/5),
-      satisfait_modes: Math.trunc(((data.reduce((total, next) => total + next.satisfait_modes, 0) / data.length)*100)/5),
-      satisfait_suivi: Math.trunc(((data.reduce((total, next) => total + next.satisfait_suivi, 0) / data.length)*100)/5),
-      satisfait_locaux: Math.trunc(((data.reduce((total, next) => total + next.satisfait_locaux, 0) / data.length)*100)/5),
+      horaire: Math.trunc(((data.reduce((total, next) => total + next.horaire, 0) / data.length) * 100) / 5),
+      charge: Math.trunc(((data.reduce((total, next) => total + next.charge, 0) / data.length) * 100) / 5),
+      satisfait_nb_matiere: Math.trunc(((data.reduce((total, next) => total + next.satisfait_nb_matiere, 0) / data.length) * 100) / 5),
+      satisfait_programme: Math.trunc(((data.reduce((total, next) => total + next.satisfait_programme, 0) / data.length) * 100) / 5),
+      satisfait_pedagogie_enseignant: Math.trunc(((data.reduce((total, next) => total + next.satisfait_pedagogie_enseignant, 0) / data.length) * 100) / 5),
+      satisfait_support: Math.trunc(((data.reduce((total, next) => total + next.satisfait_support, 0) / data.length) * 100) / 5),
+      satisfait_modes: Math.trunc(((data.reduce((total, next) => total + next.satisfait_modes, 0) / data.length) * 100) / 5),
+      satisfait_suivi: Math.trunc(((data.reduce((total, next) => total + next.satisfait_suivi, 0) / data.length) * 100) / 5),
+      satisfait_locaux: Math.trunc(((data.reduce((total, next) => total + next.satisfait_locaux, 0) / data.length) * 100) / 5),
 
-      support: Math.trunc((data.reduce((total, next) => total + (next.support ? 1 : 0), 0) / data.length)*100),
-      teams: Math.trunc((data.reduce((total, next) => total + (next.teams == "Oui" ? 1 : 0), 0) / data.length)*100),
-      ims: Math.trunc((data.reduce((total, next) => total + (next.ims == "Oui" ? 1 : 0), 0) / data.length)*100),
-      ll: Math.trunc((data.reduce((total, next) => total + (next.ll == "Oui" ? 1 : 0), 0) / data.length)*100),
-      intuns: Math.trunc((data.reduce((total, next) => total + (next.intuns == "Oui" ? 1 : 0), 0) / data.length)*100),
+      support: Math.trunc((data.reduce((total, next) => total + (next.support ? 1 : 0), 0) / data.length) * 100),
+      teams: Math.trunc((data.reduce((total, next) => total + (next.teams == "Oui" ? 1 : 0), 0) / data.length) * 100),
+      ims: Math.trunc((data.reduce((total, next) => total + (next.ims == "Oui" ? 1 : 0), 0) / data.length) * 100),
+      ll: Math.trunc((data.reduce((total, next) => total + (next.ll == "Oui" ? 1 : 0), 0) / data.length) * 100),
+      intuns: Math.trunc((data.reduce((total, next) => total + (next.intuns == "Oui" ? 1 : 0), 0) / data.length) * 100),
     }
   }
 
@@ -125,5 +125,18 @@ export class ResultatComponent implements OnInit {
     } else {
       this.updateMoyenne(this.resultats)
     }
+  }
+
+  exportToPDF(id) {
+    var element = document.getElementById(id);
+    var opt = {
+      margin: 5,
+      filename: 'QUESTIONNAIRE_DE_SATISFACTION_' + `${this.date_auj.getDate()}/${this.date_auj.getMonth() + 1}/${this.date_auj.getFullYear()}` + '.pdf',
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: 2 },
+      pagebreak:{before:'#lastQ'}
+    };
+    html2pdf().set(opt).from(element).save().then(() => {
+    });
   }
 }
