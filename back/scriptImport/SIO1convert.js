a404ee26ab7e7e2540d0226c2d34a9329a39a18f
Typlosion Flamme <pro-morganhue@outlook.fr>
2023-03-13 15:27:39 +0100

    Script Peda

--- back/scriptImport/SIO1convert.js
+++ back/scriptImport/SIO1convert.js
@@ -8,7 +8,7 @@ const { Etudiant } = require('../models/etudiant')
 let ID_SIO_1_SISR = "633d4cf5c2f7a51506d7dd74" //"640ee95d3eef9e225d77c108"
 let ID_SIO_1_SLAM = "633d4cfec2f7a51506d7dd7b" //"640ee9653eef9e225d77c10d"
 mongoose
-    .connect(`mongodb://localhost:27017/b`, {
+    .connect(`mongodb://localhost:27017/learningNode`, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -18,7 +18,7 @@ mongoose
         console.log("Start")
         var workbook = XLSX.readFile('bts_sio.xlsx');
         var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
-        console.log(xlData, xlData.length)
+        //console.log(xlData, xlData.length)
         Classe.findById(ID_SIO_1_SISR, {}, {}, (err, sisr) => {
             if (err)
                 console.error(err)
@@ -27,28 +27,37 @@ mongoose
                     console.error(err2)
                 Etudiant.find().populate('user_id').then(doc => {
                     let dicEtudiant = {}
+                    let listEmail = []
                     doc.forEach(d => {
-                        if (d && d.user_id)
+                        if (d && d.user_id) {
                             dicEtudiant[d.user_id.email] = d._id
+                        }
                     })
                     let sisrList = []
                     let slamList = []
                     xlData.forEach(data => {
+                        listEmail.push(data['EMAIL'])
                         if (dicEtudiant[data['EMAIL']]) {
                             if (data['TYPE'] && data['TYPE'].startsWith('SISR')) {
-                                Etudiant.findByIdAndUpdate(dicEtudiant[data['EMAIL']], { classe_id: sisr._id })
+                                listEmail.splice(listEmail.indexOf(data['EMAIL']))
                                 sisrList.push(dicEtudiant[data['EMAIL']])
                             }
                             else if (data['TYPE'] && data['TYPE'].startsWith('SLAM')) {
-                                slamList.push(data['EMAIL'])
-                                Etudiant.findByIdAndUpdate(dicEtudiant[data['EMAIL']], { classe_id: slam._id })
+                                listEmail.splice(listEmail.indexOf(data['EMAIL']))
+                                slamList.push(dicEtudiant[data['EMAIL']])
                             }
-                            else
-                                console.log(data?.TYPE)
+                            else if (!data['TYPE']) {
+                                listEmail.splice(listEmail.indexOf(data['EMAIL']))
+                            }
                         }
                     })
-                    console.log(slamList, slamList.length, sisrList, sisrList.length)
-                    process.exit()
+                    Etudiant.updateMany({ _id: { $in: sisrList } }, { classe_id: sisr._id }, { new: true }).then(docs => {
+                        Etudiant.updateMany({ _id: { $in: slamList } }, { classe_id: slam._id }, { new: true }).then(docs2 => {
+                            console.log(docs.n, docs2.n, "Ces emails n'ont pas pu être attribué:", listEmail)
+                            process.exit()
+                        })
+                    })
+
                 })
             })
         })
