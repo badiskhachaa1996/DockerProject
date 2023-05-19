be80365d646f83b4589d48da18b4be7ed926e048
Typlosion Flamme <pro-morganhue@outlook.fr>
2023-05-19 17:35:25 +0200

    FIX ICBS

--- back/controllers/venteCommissionController.js
+++ back/controllers/venteCommissionController.js
@@ -34,6 +34,7 @@ app.post("/getAllByPartenaireIDs", (req, res, next) => {
 });
 
 app.put("/update", (req, res) => {
+    console.log({ ...req.body })
     Vente.findByIdAndUpdate(req.body._id, { ...req.body }, (err, doc) => {
         Vente.findById(req.body._id).populate('partenaire_id').populate({ path: 'prospect_id', populate: { path: 'user_id' } }).populate('facture_id')
             .then((formFromDb) => { res.status(200).send(formFromDb); })
