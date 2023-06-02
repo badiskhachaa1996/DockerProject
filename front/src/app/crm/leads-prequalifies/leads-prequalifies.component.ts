f1311705e72b3eb0cf6cfac3e96c7ac486f309e9
Typlosion Flamme <pro-morganhue@outlook.fr>
2023-06-02 12:12:20 +0200

    Fix Externe

---
+++ front/src/app/crm/leads-prequalifies/leads-prequalifies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leads-prequalifies',
+  templateUrl: './leads-prequalifies.component.html',
+  styleUrls: ['./leads-prequalifies.component.scss']
+})
+export class LeadsPrequalifiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
