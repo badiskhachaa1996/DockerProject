9bde02227341638473620f0719a3acb9d4725584
idrissa-sall <sl.idriss10@gmail.com>
2023-07-26 11:59:01 +0200

    fron dashboard rh

--- front/src/app/rh/dashboard-rh/dashboard-rh.component.ts
+++ front/src/app/rh/dashboard-rh/dashboard-rh.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Collaborateur } from 'src/app/models/Collaborateur';
+import { DailyCheck } from 'src/app/models/DailyCheck';
+import { DailyCheckService } from 'src/app/services/daily-check.service';
+import { RhService } from 'src/app/services/rh.service';
 
 @Component({
   selector: 'app-dashboard-rh',
@@ -7,9 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardRhComponent implements OnInit {
 
-  constructor() { }
+  today: Date = new Date();
+  dailyChecks:        DailyCheck[] = [];
+  userChecksHistorique: DailyCheck[] = [];
+  collaborateurs:     Collaborateur[] = [];
+  numberOfChecks:     number = 0;
+  checkPercent:       number = 0;
+  numberOfConge:      number = 0;
+  numberOfAbsent:     number = 0;
+  numberOfPause:      number = 0;
+  numberOfDisponible: number = 0;
+  numberOfReunion:    number = 0;
+  numberOfOccupe:     number = 0;
+
+  loading:boolean = true;
+  statutList: any[] = [
+    { label: 'En congé', value: 'En congé' },
+    { label: 'Disponible', value: 'Disponible' },
+    { label: 'En réunion', value: 'En réunion' },
+    { label: 'Occupé', value: 'Occupé' },
+    { label: 'Absent', value: 'Absent' },
+    { label: 'En pause', value: 'En pause' },
+  ];
+
+  constructor(private rhService: RhService, private dailyCheckService: DailyCheckService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    // recuperation de la liste des checks
+    this.onGetUsersDailyChecksAndCollaborateur();
   }
+
+  // recuperation de la liste des checks du jours et des collaborateurs
+  onGetUsersDailyChecksAndCollaborateur(): void
+  {
+    // recuperation de la liste des checks
+    this.dailyCheckService.getAllUsersDailyChecks()
+    .then((response) => {
+      this.dailyChecks = response;
+      // nombre de checks
+      this.numberOfChecks = this.dailyChecks.length;
+
+      // recuperation de la liste des collaborateurs
+      this.rhService.getCollaborateurs()
+      .then((response) => {
+        this.collaborateurs = response;
+        console.log(response);
+        // pourcentage de checks
+        this.checkPercent = Math.ceil((this.collaborateurs.length * this.numberOfChecks));
+
+        // initialisation à zero
+        this.numberOfDisponible = 0;
+        this.numberOfConge      = 0;
+        this.numberOfReunion    = 0;
+        this.numberOfOccupe     = 0;
+        this.numberOfAbsent     = 0;
+        this.numberOfPause      = 0;
+
+        // remplissage des variables de statuts
+        this.collaborateurs.forEach((collaborateur: Collaborateur) => {
+          const {user_id}: any = collaborateur;
+
+          if(user_id.statut == 'Disponible')
+          {
+            this.numberOfDisponible ++;
+          } else if(user_id.statut == 'En congé')
+          {
+            this.numberOfConge ++;
+          } else if(user_id.statut == 'En réunion')
+          {
+            this.numberOfReunion ++;
+          } else if(user_id.statut == 'Occupé')
+          {
+            this.numberOfOccupe ++;
+          } else if(user_id.statut == 'Absent')
+          {
+            this.numberOfAbsent ++;
+          } else if(user_id.statut == 'En pause')
+          {
+            this.numberOfPause ++;
+          }
+        });
+      })
+      .catch((error) => { this.messageService.add({severity: 'error', summary: 'Erreur système', detail: 'Impossible de récupérer la liste des présences'}); });
+    })
+    .catch((error) => { this.messageService.add({severity: 'error', summary: 'Erreur système', detail: 'Impossible de récupérer la liste des présences'}) });
+  }
+
 
 }
