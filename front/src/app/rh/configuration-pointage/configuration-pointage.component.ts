343cee674bce4bfa7304be9628174e8ee139a21e
unknown <pro-morganhue@outlook.fr>
2023-09-01 15:47:16 +0200

    Affichage des users

--- front/src/app/rh/configuration-pointage/configuration-pointage.component.ts
+++ front/src/app/rh/configuration-pointage/configuration-pointage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PointageService } from 'src/app/services/pointage.service';
 import { PointeuseService } from 'src/app/services/pointeuse.service';
+import { RhService } from 'src/app/services/rh.service';
 
 @Component({
   selector: 'app-configuration-pointage',
@@ -9,13 +10,25 @@ import { PointeuseService } from 'src/app/services/pointeuse.service';
 })
 export class ConfigurationPointageComponent implements OnInit {
 
-  constructor(private PoiService: PointeuseService, private PointageService: PointageService) { }
+  constructor(private PoiService: PointeuseService, private PointageService: PointageService, private rhService: RhService) { }
   machineList = []
   pointages = {}
+  collaborateurDic = {}
+  uidDic = {}
 
   ngOnInit(): void {
     this.PoiService.getAll().subscribe(ps => {
       this.machineList = ps
+    })
+    this.PoiService.getData().subscribe(pd => {
+      pd.forEach(p => {
+        p.users.forEach(u => {
+          if (u.user_id) {
+            this.uidDic[u.UID] = u.user_id
+          }
+        })
+      })
+      console.log(this.uidDic)
     })
     this.PointageService.getAll().subscribe(ps => {
       console.log(ps)
@@ -27,6 +40,15 @@ export class ConfigurationPointageComponent implements OnInit {
         }
       })
     })
+    this.rhService.getCollaborateurs()
+      .then((response) => {
+
+        response.forEach(c => {
+          if (c.user_id)
+            this.collaborateurDic[c.user_id._id] = `${c.user_id.lastname} ${c.user_id.firstname}`
+        })
+        console.log(this.collaborateurDic)
+      })
   }
 
 }
