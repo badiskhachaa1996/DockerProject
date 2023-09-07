004ff3566ff73600a7b24f998ede1c606ae5b645
unknown <pro-morganhue@outlook.fr>
2023-09-07 11:34:01 +0200

    Archivage des pointages + Espace Personnel

--- front/src/app/rh/archivage-pointage/archivage-pointage.component.ts
+++ front/src/app/rh/archivage-pointage/archivage-pointage.component.ts
@@ -4,6 +4,9 @@ import { PointageService } from 'src/app/services/pointage.service';
 import { PointeuseService } from 'src/app/services/pointeuse.service';
 import { RhService } from 'src/app/services/rh.service';
 
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
+
 @Component({
   selector: 'app-archivage-pointage',
   templateUrl: './archivage-pointage.component.html',
@@ -60,12 +63,45 @@ export class ArchivagePointageComponent implements OnInit {
       })
   }
 
+  dataHisto = {}
+  display = false
   seeHisto(pds: PointageData[]) {
-
+    this.dataHisto = pds
+    pds.forEach(p => {
+      if (this.dataHisto[p.machine]) {
+        this.dataHisto[p.machine].push(p)
+      } else {
+        this.dataHisto[p.machine] = [p]
+      }
+      this.date_dernier_maj = new Date(p.updateDate)
+    })
+    this.display = true
   }
 
-  exportToExcel(pds: PointageData[]) {
-
+  exportToExcel(pds: PointageData[], date) {
+    let dataExcel = []
+    //Clean the data
+    pds.forEach(p => {
+      let t = {}
+      let c = "Non Affecté"
+      if (this.collaborateurDic && this.uidDic && p.machine && this.uidDic[p.machine] && this.uidDic[p.machine][p.uid] && this.collaborateurDic[this.uidDic[p.machine][p.uid]]) {
+        c = this.collaborateurDic[this.uidDic[p.machine][p.uid]]
+      }
+      t['Machine'] = p?.machine
+      t['Localisation'] = this.machineDic[p?.machine]?.localisation
+      t['Date'] = p?.date
+      t['Type'] = p?.type
+      t['UID'] = p?.uid
+      t['Collaborateur '] = c
+      dataExcel.push(t)
+    })
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataExcel);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    const data: Blob = new Blob([excelBuffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
+    });
+    FileSaver.saveAs(data, "pointage_" + date + '_export_' + new Date().toLocaleDateString("fr-FR") + ".xlsx");
   }
 
 }
