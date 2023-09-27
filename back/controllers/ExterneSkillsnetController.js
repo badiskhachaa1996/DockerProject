6d70ad3ad73dc177d87d6104d2ec701b2fad092a
unknown <pro-morganhue@outlook.fr>
2023-09-27 16:25:23 +0200

    Calendrier disponibilités

--- back/controllers/ExterneSkillsnetController.js
+++ back/controllers/ExterneSkillsnetController.js
@@ -28,58 +28,70 @@ if (process.argv[2]) {
 }
 
 app.post('/create/:id', (req, res) => {
-    let newUser = new User({
-        ...req.body
-    })
-
-    let passwordClear = "INTED@" + req.body.lastname + new Date().getSeconds().toString()
-    newUser.password = bcrypt.hashSync(passwordClear, 8)
-    newUser.type = "Externe-InProgress"
-    //newUser.verifedEmail = true
-    let created_by = req.params?.id
-
-    newUser.save().then(userCreated => {
-        if (created_by == null || created_by == "null")
-            created_by = userCreated._id
-        let newObj = new ExterneSkillsnet({
-            user_id: userCreated._id,
-            created_by,
-            created_at: new Date()
-        })
-        newObj.save().then(doc => {
-            ExterneSkillsnet.findById(doc._id).populate('user_id').populate('created_by').then(newDoc => {
-                let htmlmail = `
-                <p style="font-size:20px; color:black">Bonjour,</p>
-                <p>   </p>
-                <p style="font-size:20px; color:black">Votre compte SkillsNet a été crée sur la plateforme <a href='${origin[0]}/#/formulaire-externe'>IMS</a>.</p>
-                <p style="font-size:20px; color:black">Merci de compléter votre profil sur <a href='${origin}/#/formulaire-externe'>cette page</a></p>
-                <p style="font-size:20px; color:black">Vos identifiants sont votre adresse email et votre mot de passe est : <strong>${passwordClear}</strong></p>
-                <p>   </p>
-                <p style="font-size:20px;">Cordialement,</p>
-                
-                <footer> <img src="cid:red"/></footer>
-                `
-                let attachments = [{
-                    filename: 'nop.png',
-                    path: 'assets/ims-intedgroup-logo.png',
-                    cid: 'red' //same cid value as in the html img src
-                }]
-                let mailOptions = {
-                    from: 'ims@intedgroup.com',
-                    to: newUser.email_perso,
-                    subject: '[IMS] Création de compte',
-                    html: htmlmail,
-                    attachments: attachments
-                };
-                /*transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.error(error);
-                    }
-                });*/
-                res.send(newDoc)
+    User.findOne({ email_perso: req.body.email_perso }).then(userFind => {
+        if (!userFind) {
+            let newUser = new User({
+                ...req.body
             })
-        })
+
+            let passwordClear = "INTED@" + req.body.lastname + new Date().getSeconds().toString()
+            newUser.password = bcrypt.hashSync(passwordClear, 8)
+            newUser.type = "Externe-InProgress"
+            //newUser.verifedEmail = true
+            let created_by = req.params?.id
+
+            newUser.save().then(userCreated => {
+                if (created_by == null || created_by == "null")
+                    created_by = userCreated._id
+                let newObj = new ExterneSkillsnet({
+                    user_id: userCreated._id,
+                    created_by,
+                    created_at: new Date()
+                })
+                newObj.save().then(doc => {
+                    ExterneSkillsnet.findById(doc._id).populate('user_id').populate('created_by').then(newDoc => {
+                        let htmlmail = `
+                        <p style="font-size:20px; color:black">Bonjour,</p>
+                        <p>   </p>
+                        <p style="font-size:20px; color:black">Votre compte SkillsNet a été crée sur la plateforme <a href='${origin[0]}/#/formulaire-externe'>IMS</a>.</p>
+                        <p style="font-size:20px; color:black">Merci de compléter votre profil sur <a href='${origin}/#/formulaire-externe'>cette page</a></p>
+                        <p style="font-size:20px; color:black">Vos identifiants sont votre adresse email et votre mot de passe est : <strong>${passwordClear}</strong></p>
+                        <p>   </p>
+                        <p style="font-size:20px;">Cordialement,</p>
+                        
+                        <footer> <img src="cid:red"/></footer>
+                        `
+                        let attachments = [{
+                            filename: 'nop.png',
+                            path: 'assets/ims-intedgroup-logo.png',
+                            cid: 'red' //same cid value as in the html img src
+                        }]
+                        let mailOptions = {
+                            from: 'ims@intedgroup.com',
+                            to: newUser.email_perso,
+                            subject: '[IMS] Création de compte',
+                            html: htmlmail,
+                            attachments: attachments
+                        };
+                        /*transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                console.error(error);
+                            }
+                        });*/
+                        res.send(newDoc)
+                    })
+                })
+            })
+        } else {
+            ExterneSkillsnet.findOne({ user_id: userFind._id }).populate('user_id').populate('created_by').then(newDoc => {
+                if (newDoc)
+                    res.send(newDoc)
+                else
+                    res.send({user_id:userFind})
+            })
+        }
     })
+
 
 })
 
