49e45ab25ff2db9081e3fb43fa0211443c72f993
ferhat75 <mezifarhat1999@gmail.com>
2023-10-04 16:45:26 +0200

    links

--- back/controllers/linksController.js
+++ back/controllers/linksController.js
@@ -10,9 +10,30 @@ app.post("/create-links", (req, res) => {
     .then((linksCreated) => { res.status(201).json({ links: linksCreated, success: 'links crée' }) })
     .catch((error) => { console.error(error); res.status(500).json({ error: 'Impossible de créer le link, si le problème persite veuillez créer un ticket au service IMS' }); })
 });
+
+//recuperation des links
+
+app.get("/getalllinks", (req, res) => {
+    Links.find()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("impossible de récupérer les links");
+    })});
+
+
 app.get('/get-links/:id', (req, res, next) => {
     Links.find({ user_id: req.params.id })
     .then((links) => { res.status(200).send(links); })
     .catch((error) => { console.error(error); res.status(400).json({ error: 'Impossible de récuperer le lien'}); })
+});
+app.put("/put-links", (req, res, next) => {
+    const links = new Links({ ...req.body });
+
+    Links.updateOne({ _id: links._id }, { ...req.body })
+    .then((links) => { res.status(201).json({ links: links, success: 'Links mis à jour'}); })
+    .catch((error) => { console.error(error); res.status(400).json({ error: 'Impossible de mettre à jour le links, si le problème persite veuillez créer un ticket au service IMS' }); });
 });
 module.exports = app;
