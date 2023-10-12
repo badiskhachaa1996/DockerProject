f28ab5c18fadbedbfb8470098329302455f4124c
unknown <pro-morganhue@outlook.fr>
2023-10-12 11:57:28 +0200

    Offre retour

--- front/src/app/skillsnet/cvtheque/cvtheque.component.ts
+++ front/src/app/skillsnet/cvtheque/cvtheque.component.ts
@@ -518,8 +518,13 @@ export class CvthequeComponent implements OnInit {
   })
 
   onAssignOffer() {
+    let type_matching = "Candidat"
+    if (this.token.type == "CEO Entreprise" || this.token.type == "Entreprise" || this.token.type == "Tuteur")
+      type_matching = "Entreprise"
+    else if (this.token.type == "Commercial")
+      type_matching = "Commercial"
     this.selectedCVs.forEach((cv: any) => {
-      this.MatchingService.create(new Matching(null, this.AssignForm.value.offer, this.token.id, cv._id, "En Cours", "Etudiant", new Date())).subscribe(match => {
+      this.MatchingService.create(new Matching(null, this.AssignForm.value.offer, this.token.id, cv._id, "En cours", type_matching, new Date())).subscribe(match => {
         this.messageService.add({ severity: 'success', summary: "Création du Matching avec " + cv.user_id?.lastname + " " + cv.user_id?.firstname })
       }, error => {
         this.messageService.add({ severity: 'error', summary: "Error sur le Matching avec " + cv.user_id?.lastname + " " + cv.user_id?.firstname, detail: error.toString() })
