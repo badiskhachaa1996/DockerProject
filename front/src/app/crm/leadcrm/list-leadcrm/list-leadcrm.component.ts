5c2983db312e1cc2de3b8c66bac106eb13fd463e
Nazif toure <naziftoure14@gmail.com>
2023-11-21 17:36:42 +0100

    Ajout de la fonction envoi d'email à un lead

--- front/src/app/crm/leadcrm/list-leadcrm/list-leadcrm.component.ts
+++ front/src/app/crm/leadcrm/list-leadcrm/list-leadcrm.component.ts
@@ -14,6 +14,14 @@ import {ProductService} from "../../../dev-components/service-template/productse
 import {GestionProduitsService} from "../../gestion-produits/gestion-produits.service";
 import {ProduitCRM} from "../../../models/produitCRM";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Prospect} from "../../../models/Prospect";
+import {EmailTypeService} from "../../../services/email-type.service";
+import {MailType} from "../../../models/MailType";
+import {HistoriqueEmail} from "../../../models/HistoriqueEmail";
+import {HistoriqueLead} from "../../../models/HistoriqueLead";
+import { FileUpload } from 'primeng/fileupload';
+import {AdmissionService} from "../../../services/admission.service";
+
 @Component({
   selector: 'app-list-leadcrm',
   templateUrl: './list-leadcrm.component.html',
@@ -85,9 +93,12 @@ export class ListLeadcrmComponent implements OnInit {
     produitList = [];
     private selectedLead: LeadCRM;
 
-
   constructor(private LCS: LeadcrmService, private ToastService: MessageService, private FAService: FormulaireAdmissionService,
-    private TeamCRMService: TeamsCrmService, private UserService: AuthService, private Products : GestionProduitsService, private router: Router, private route: ActivatedRoute) { }
+    private TeamCRMService: TeamsCrmService, private UserService: AuthService, private Products : GestionProduitsService, private router: Router, private route: ActivatedRoute,
+              private EmailTypeS: EmailTypeService,
+              private messageService: MessageService,
+              private admissionService: AdmissionService,
+              ) { }
   leads: LeadCRM[] = []
   ngOnInit(): void {
     this.LCS.getAll().subscribe(data => {
@@ -366,6 +377,155 @@ export class ListLeadcrmComponent implements OnInit {
     }
 
 
+    // Gestion des emails envoyé un email, afficher les emails envoyés et les emails types
 
+  showEmail = false
+  leadSendTo: LeadCRM = null
+  emailTypeSelected: string = null
+  mailDropdown = []
+  mailTypeDropdown = []
+  @ViewChild('fileInput') fileInput: FileUpload;
+
+  formEmailPerso = new FormGroup({
+    objet: new FormControl('', Validators.required),
+    body: new FormControl('', Validators.required),
+    cc: new FormControl([]),
+    send_from: new FormControl('', Validators.required)
+  })
+  formEmailType = new FormGroup({
+    objet: new FormControl('', Validators.required),
+    body: new FormControl('', Validators.required),
+    cc: new FormControl([]),
+    send_from: new FormControl('', Validators.required)
+  })
+  onEmailPerso() {
+    console.log(this.formEmailPerso.value.cc)
+    this.EmailTypeS.sendPerso({ ...this.formEmailPerso.value, send_by: this.token.id, send_to: this.leadSendTo.email, send_from: this.formEmailPerso.value.send_from._id, pieces_jointes: this.piece_jointes, mailTypeSelected: this.mailTypeSelected }).subscribe(data => {
+      this.messageService.add({ severity: "success", summary: 'Envoie du mail avec succès' })
+      this.EmailTypeS.HEcreate({ ...this.formEmailPerso.value, send_by: this.token.id, send_to: this.leadSendTo._id, send_from: this.formEmailPerso.value.send_from.email }).subscribe(data2 => {
+        this.formEmailPerso.reset()
+        this.historiqueEmails.push(data2)
+        this.messageService.add({ severity: "success", summary: 'Enregistrement de l\'envoie du mail avec succès' })
+      })
+    })
+
+  }
+  onEmailType() {
+    this.EmailTypeS.sendPerso({ ...this.formEmailType.value, send_by: this.token.id, send_to: this.leadSendTo.email, send_from: this.formEmailType.value.send_from._id, pieces_jointes: this.piece_jointes, mailTypeSelected: this.mailTypeSelected }).subscribe(data => {
+      this.messageService.add({ severity: "success", summary: 'Envoie du mail avec succès' })
+      this.EmailTypeS.HEcreate({ ...this.formEmailType.value, send_by: this.token.id, send_to: this.leadSendTo._id, send_from: this.formEmailType.value.send_from.email }).subscribe(data2 => {
+        this.formEmailType.reset()
+        this.historiqueEmails.push(data2)
+        this.messageService.add({ severity: "success", summary: 'Enregistrement de l\'envoie du mail avec succès' })
+      })
+    })
+
+  }
+  initSendEmail(lead: LeadCRM) {
+    this.showEmail = true
+    this.leadSendTo = lead
+    console.log(this.leadSendTo)
+    this.EmailTypeS.HEgetAllTo(this.leadSendTo._id).subscribe(data => {
+      this.historiqueEmails = data
+    })
+    this.EmailTypeS.getAll().subscribe(data => {
+      data.forEach(val => {
+        this.mailDropdown.push({ label: val.email, value: val })
+      })
+    })
+    this.EmailTypeS.MTgetAll().subscribe(data => {
+      data.forEach(e => {
+        this.mailTypeDropdown.push({ label: e.objet, value: e })
+      })
+    })
+  }
+
+  onMailType(event: MailType) {
+    this.formEmailType.patchValue({
+      ...event
+    })
+    this.piece_jointes = event.pieces_jointe
+    this.mailTypeSelected = event
+  }
+  mailTypeSelected: MailType
+  historiqueEmails: HistoriqueEmail[] = []
+  piece_jointes = []
+
+  //Gestion des PJs
+  onDeletePJ(ri) {
+    delete this.piece_jointes[ri]
+  }
+
+  uploadFilePJ: {
+    date: Date,
+    nom: string,
+    path: string,
+    _id: string
+  } = null
+
+  onAddPj() {
+    this.piece_jointes.push({ date: new Date(), nom: "Téléverser le fichier s'il vous plaît", path: '', _id: new mongoose.Types.ObjectId().toString() })
+  }
+  downloadPJFile(pj) {
+    this.EmailTypeS.downloadPJ(this.mailTypeSelected?._id, pj._id, pj.path).subscribe((data) => {
+      const byteArray = new Uint8Array(atob(data.file).split('').map(char => char.charCodeAt(0)));
+      var blob = new Blob([byteArray], { type: data.documentType });
+      saveAs(blob, pj.path)
+    }, (error) => {
+      console.error(error)
+    })
+  }
+
+  onUploadPJ(uploadFilePJ) {
+    if (uploadFilePJ?.nom && uploadFilePJ.nom != 'Cliquer pour modifier le nom du document ici') {
+      document.getElementById('selectedFile').click();
+      this.uploadFilePJ = uploadFilePJ
+    } else {
+      this.messageService.add({ severity: 'error', summary: 'Vous devez d\'abord donner un nom au fichier avant de l\'upload' });
+    }
+
+  }
+
+
+
+  FileUploadPJ(event: [File]) {
+    console.log(event)
+    if (event != null) {
+      this.messageService.add({ severity: 'info', summary: 'Envoi de Fichier', detail: 'Envoi en cours, veuillez patienter ...' });
+      const formData = new FormData();
+      formData.append('nom', this.uploadFilePJ.nom)
+      formData.append('pj_id', this.uploadFilePJ._id)
+      formData.append('path', event[0].name)
+      formData.append('_id', this.mailTypeSelected?._id)
+      formData.append('file', event[0])
+      this.EmailTypeS.uploadPJ(formData).subscribe(res => {
+        this.messageService.add({ severity: 'success', summary: 'Envoi de Fichier', detail: 'Le fichier a bien été envoyé' });
+        this.piece_jointes[this.piece_jointes.indexOf(this.uploadFilePJ)].path = event[0].name
+        this.uploadFilePJ = null;
+        this.fileInput.clear()
+      }, error => {
+        this.messageService.add({ severity: 'error', summary: 'Envoi de Fichier', detail: 'Une erreur est arrivé' });
+      });
+    }
+  }
+  listHistorique: HistoriqueLead[] = []
+  leadHistorique: Prospect
+  initHistorique(lead: Prospect) {
+    this.leadHistorique = lead
+    this.admissionService.getAllHistoriqueFromLeadID(lead._id).subscribe(data => {
+      this.listHistorique = data
+    })
+  }
+
+  onHideEmailForm() {
+    this.showEmail = false
+    this.leadSendTo = null
+    this.mailDropdown = []
+    this.mailTypeDropdown = []
+    this.historiqueEmails = []
+    this.piece_jointes = []
+    this.formEmailPerso.reset()
+    this.formEmailType.reset()
+  }
 
 }
